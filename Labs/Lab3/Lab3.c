e33680645262ccc144c79d9a9f41d14c5fd97bae
Terence Tan <Terence@Terences-Mac-mini.local>
2018-10-16 20:37:20 -0700

    basic template laid out

---
+++ Labs/Lab3/Lab3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	printf("This is working\n");
+}
+
+int CountSetBits(unsigned int var) {
+	
+	return var;
+}
+
+int IdentifyUniqueInteger(int arrayCount, int* intArray) {
+	return 0;
+}
+
+bool OnlyOneBitSet(int a) {
+	return 0;
+}
+
+bool OnlyOneBitSetInEvenPosition(int b) {
+	return 0;
+}
+
+int ModWithoutUsingModOperator(int numerator, int denominator) {
+	return 0;
+}
+
+int SwapNibbles(int c) {
+	return 0;
+}
+
+int NumberOfOPerationsREquire(int d) {
+	return 0;
+}
+
