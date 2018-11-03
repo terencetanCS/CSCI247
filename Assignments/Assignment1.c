f18dbb6ee2c5583406b3037fcdb780fc9d9fbe01
Terence Tan <tant@wwu.edu>
2018-11-03 16:29:09 -0700

    task 3 is done i think

--- Assignments/Assignment1.c
+++ Assignments/Assignment1.c
@@ -1,22 +1,41 @@
 //assignment 1
+//Note: I used cc -pthread Assignment1.c to compile
+//and ./a.out to run since that is what was used on
+//the linux tutorial. gcc Assignment1.c -lpthread also
+//seems to compile
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 void *print_message_function(void *ptr);
 
+typedef struct thread {
+  unsigned long int threadID; //use pthread_self()
+  char policy[12];
+  int priority;
+  char startTime[100];
+  char endTime[100];
+  long int startTimeStamp;
+  long int endTimeStamp;
+} statistics[9];
+
 int main() {
-  pthread_t thread1;
-  const char *message1 = "Thread 1";
-  int iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
-  if(iret1) {
+  statistics threadstats;
+  pthread_t mainThread,thread1;
+  mainThread = pthread_self();
+  const char *message1 = "Main Thread";
+  const char *message2 = "thread 1";
+  int iret1 = pthread_create( &mainThread, NULL, print_message_function, (void*) message1);
+  int iret2 = pthread_create( &mainThread, NULL, print_message_function, (void*) message2);
+  /*if(iret1) {
     fprintf(stderr, "Error - pthread_create() return code: %d\n", iret1);
     exit(EXIT_FAILURE);
-  }
+  }*/
+  pthread_join(mainThread, NULL);
   pthread_join(thread1, NULL);
   exit(EXIT_SUCCESS);
 }
 
 void *print_message_function(void *ptr) {
-  char *message = (char *) ptr;
-  printf("%s\n", message);
+  printf("%lu\n",pthread_self());
+  return NULL;
 }
